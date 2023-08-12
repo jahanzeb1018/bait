ff46513210ccfe7948fb0fdf215ad3a644893247
jahanzeb1000 <mohammad.jahanzeb01@estudiant.upf.edu>
2023-08-12 16:33:59 +0200

    v1 Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -10,17 +10,21 @@ import {useRouter} from 'next/router';
 // framer motion
 import { AnimatePresence, motion} from 'framer-motion';
 
+// analytics
+import { Analytics } from '@vercel/analytics/react';
+
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return( 
-  <Layout>
-    <AnimatePresence mode='wait'>
-      <motion.div key={router.route} className='h-full'>
-        <Transition />
-        <Component {...pageProps} />
-      </motion.div>
-    </AnimatePresence>
-  </Layout>
+    <Layout>
+      <AnimatePresence mode='wait'>
+        <motion.div key={router.route} className='h-full'>
+          <Transition />
+          <Component {...pageProps} />
+          <Analytics />
+        </motion.div>
+      </AnimatePresence>
+    </Layout>
   );
 }
 
